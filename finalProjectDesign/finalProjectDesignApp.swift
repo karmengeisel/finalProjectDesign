362c36331efa5df8f72ebf1b422b88233a21d1a9
Karmen <karmengeisel@icloud.com>
2023-06-27 15:28:45 -0600

    Initial Commit

---
+++ finalProjectDesign/finalProjectDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  finalProjectDesignApp.swift
+//  finalProjectDesign
+//
+//  Created by Scholar on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct finalProjectDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
